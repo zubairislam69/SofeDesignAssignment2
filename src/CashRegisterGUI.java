af7b1a6bc70f0bca6e3b306c2fa6d2d3a9233501
zubxxr <zubxxr@gmail.com>
2022-10-30 17:26:58 -0400

    Fixed

--- src/CashRegisterGUI.java
+++ src/CashRegisterGUI.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,15 +15,20 @@ import javax.swing.JTextField;
 public class CashRegisterGUI implements ActionListener {
     private static JButton startButton, enterButton, checkoutButton, 
             authorizeButton, endSessionButton, cancelSessionButton ;    
-    private static JTextField paymentMethodInput, prodPriceInput, prodNameInput ;
-    private static JLabel prodName, prodPrice, paymentMethod;;                   
+    private static JTextField paymentMethodInput, barcodeInput ;
+    private static JLabel prodName, prodPrice, paymentMethod, barcode;
+    private static JLabel barcodeError = new JLabel();
+    private static JLabel errorMessage = new JLabel();
+    private static JLabel price = new JLabel();
     private static JPanel panel = new JPanel();
     private static JFrame frame = new JFrame();
-    private static double totalPrice = 0;
+    
     private static int p = 20;
+    private static double totalPrice = 0;
+    private static final DecimalFormat decfor = new DecimalFormat("0.00");
 
     public static void main(String[] args) {  
-        frame.setSize(500, 200);
+        frame.setSize(300, 200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
         
@@ -40,24 +46,26 @@ public class CashRegisterGUI implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == startButton) {
             panel.removeAll();
-            frame.setSize(1000, 300); //re-render
+            frame.setSize(1000, 500); //re-render
 
-            prodName = new JLabel("Product name:");
-            prodName.setBounds(10, 20, 130, 25);
-            panel.add(prodName);
+            JLabel column1;
+            column1 = new JLabel("Product Name");
+            column1.setBounds(400, 15, 200, 25);
+            panel.add(column1);
+            
+            JLabel column2;
+            column2 = new JLabel("Product Price");
+            column2.setBounds(550, 15, 200, 25);
+            panel.add(column2);
 
-            prodNameInput = new JTextField();
-            prodNameInput.setBounds(110, 20, 165, 25);
-            panel.add(prodNameInput);
-
-            prodPrice = new JLabel("Product price:");
-            prodPrice.setBounds(10, 50, 130, 25);
-            panel.add(prodPrice);
- 
-            prodPriceInput = new JTextField();
-            prodPriceInput.setBounds(110, 50, 165, 25);
-            panel.add(prodPriceInput);
-                        
+            barcode = new JLabel("Barcode:");
+            barcode.setBounds(10, 20, 130, 25);
+            panel.add(barcode);
+            
+            barcodeInput = new JTextField();
+            barcodeInput.setBounds(80, 20, 165, 25);
+            panel.add(barcodeInput);
+       
             enterButton = new JButton("Enter");
             enterButton.setBounds(10, 80, 80, 25);
             enterButton.addActionListener(new CashRegisterGUI());
@@ -71,7 +79,7 @@ public class CashRegisterGUI implements ActionListener {
             cancelSessionButton = new JButton("Cancel Session");
             cancelSessionButton.setBounds(10, 200, 160, 25);
             cancelSessionButton.addActionListener(new CashRegisterGUI());
-            panel.add(cancelSessionButton);   
+            panel.add(cancelSessionButton);    
         }
         
         else if (e.getSource() == cancelSessionButton) {
@@ -79,7 +87,7 @@ public class CashRegisterGUI implements ActionListener {
             panel.removeAll();
             totalPrice = 0;
             
-            frame.setSize(1000, 200);
+            frame.setSize(1001, 500);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.add(panel);
 
@@ -93,54 +101,90 @@ public class CashRegisterGUI implements ActionListener {
             frame.setVisible(true);
         }
             
-        else if (e.getSource() == enterButton) {    //sadasdsadasd 
-            p += 15;
+        else if (e.getSource() == enterButton) {
             
-            frame.setSize(1001, 300); //re-render
-            totalPrice += Double.parseDouble(prodPriceInput.getText());
- 
-            JLabel column1;
-            column1 = new JLabel("Product Name");
-            column1.setBounds(400, 15, 200, 25);
-            panel.add(column1);
+            prodPrice = new JLabel();
+            prodName = new JLabel();
             
-            JLabel column2;
-            column2 = new JLabel("Product Price");
-            column2.setBounds(550, 15, 200, 25);
-            panel.add(column2);
-                   
-            JLabel ticketProdName;
-            ticketProdName = new JLabel();
-            ticketProdName.setBounds(400, p, 200, 25);
-            ticketProdName.setText(prodNameInput.getText());
-            panel.add(ticketProdName);
-
-            JLabel ticketProdPrice;
-            ticketProdPrice = new JLabel();
-            ticketProdPrice.setBounds(550, p, 130, 25);
-            ticketProdPrice.setText(prodPriceInput.getText());
-            panel.add(ticketProdPrice);
       
-            frame.setSize(1002, 300); //re-render
-   
-            prodPriceInput.setText("");
-            prodNameInput.setText("");
+            barcodeError.setBounds(10, 100, 250, 25);
+            panel.add(barcodeError);
+
+            frame.setSize(1002, 500); //re-render
+
+            switch (barcodeInput.getText()) {
+                case "123" -> {
+                    barcodeError.setText("");
+                    p += 15;
+                    prodName.setBounds(400, p, 130, 25);
+                    prodPrice.setBounds(550, p, 130, 25);
+
+                    prodName.setText("Apples");
+                    panel.add(prodName);
+                    prodPrice.setText("4.47");
+                    panel.add(prodPrice);                     
+                }
+                case "456" -> { 
+                    barcodeError.setText("");
+                    p += 15;
+                    prodName.setBounds(400, p, 130, 25);
+                    prodPrice.setBounds(550, p, 130, 25);
+
+                    prodName.setText("Banana");
+                    panel.add(prodName);
+                    prodPrice.setText("0.40");
+                    panel.add(prodPrice);
+                }
+                case "789" -> { 
+                    barcodeError.setText("");
+                    p += 15;
+                    prodName.setBounds(400, p, 130, 25);
+                    prodPrice.setBounds(550, p, 130, 25);
+
+                    prodName.setText("Raspberries");
+                    panel.add(prodName);
+                    prodPrice.setText("2.97");
+                    panel.add(prodPrice);
+                }
+                case "012" -> {
+                    barcodeError.setText("");
+                    p += 15;
+                    prodName.setBounds(400, p, 130, 25);
+                    prodPrice.setBounds(550, p, 130, 25);
+
+                    prodName.setText("Blueberries");
+                    panel.add(prodName);
+                    prodPrice.setText("5.97");
+                    panel.add(prodPrice);
+                }                   
+                default -> {
+                    barcodeError.setText("Unknown Product");
+                    frame.setSize(1002, 500); //re-render
+                }
+            }
+            
+            barcodeInput.setText("");
+
+                        
+                        
+            if (!prodPrice.getText().equals("")) {
+                totalPrice += Double.parseDouble(prodPrice.getText());
+            }
+
+           frame.setSize(1003, 500); //re-render  
         }
         
         else if (e.getSource() == checkoutButton) {   
-            frame.setSize(1003, 300); //re-render
+            frame.setSize(1004, 500); //re-render
 
             panel.remove(enterButton);
-            panel.remove(prodName);
-            panel.remove(prodPrice);
-            panel.remove(prodNameInput);
-            panel.remove(prodPriceInput);
             panel.remove(checkoutButton);
-          
-            JLabel price;
-            price = new JLabel();
+            panel.remove(barcodeInput);
+            panel.remove(barcode);
+            panel.remove(barcodeError);
+       
             price.setBounds(10, 20, 130, 25);
-            price.setText("Total Price: " + totalPrice);
+            price.setText("Total Price: " + decfor.format(totalPrice));
             panel.add(price);
                      
             paymentMethod = new JLabel("Add Payment Method (Cash, Debit, or Credit): ");
@@ -158,6 +202,11 @@ public class CashRegisterGUI implements ActionListener {
         }
                
         else if (e.getSource() == authorizeButton) {
+            
+            frame.setSize(1005, 500); //re-render
+            errorMessage.setBounds(10, 100, 250, 25);
+            errorMessage.setText("Please enter correct payment method");
+                
             if (paymentMethodInput.getText().equals("Cash") || 
                     paymentMethodInput.getText().equals("Debit") || 
                             paymentMethodInput.getText().equals("Credit"))
@@ -166,28 +215,24 @@ public class CashRegisterGUI implements ActionListener {
                 panel.remove(paymentMethodInput);
                 panel.remove(paymentMethod);
                 panel.remove(cancelSessionButton);
-
+                panel.remove(barcodeError);
+                panel.remove(errorMessage);
+                                
                 JLabel reciept;
                 reciept = new JLabel("Reciept");
                 reciept.setBounds(490, 0, 200, 25);
                 panel.add(reciept);
-            
-                frame.setSize(1004, 300); //re-render
-
+         
+                price.setBounds(480, p+50, 130, 25);
+    
                 JLabel authMessage;
                 authMessage = new JLabel();
                 authMessage.setBounds(10, 40, 250, 25);
-
-                if (paymentMethodInput.getText().equals("Cash")){
-                    authMessage.setText("Payment authorized using cash");
-                }
-
-                else if (paymentMethodInput.getText().equals("Debit")){
-                    authMessage.setText("Payment authorized using Debit Card");
-                }
                 
-                else if (paymentMethodInput.getText().equals("Credit")){
-                    authMessage.setText("Payment authorized using Credit Card");
+                switch (paymentMethodInput.getText()) {
+                    case "Cash" -> authMessage.setText("Payment authorized using cash");
+                    case "Debit" -> authMessage.setText("Payment authorized using Debit Card");
+                    case "Credit" -> authMessage.setText("Payment authorized using Credit Card");
                 }
 
                 panel.add(authMessage);
@@ -198,19 +243,16 @@ public class CashRegisterGUI implements ActionListener {
                 panel.add(endSessionButton);
             }
             else {
-                frame.setSize(1005, 300); //re-render
-
-                JLabel errorMessage;
-                errorMessage = new JLabel("Please enter correct payment method");
-                errorMessage.setBounds(10, 100, 250, 25);
+                frame.setSize(1006, 500); //re-render
                 panel.add(errorMessage);
             }
         }
         
         else if (e.getSource() == endSessionButton) {
             p = 20;
+            totalPrice = 0; 
             panel.removeAll();
-            frame.setSize(1000, 200);
+            frame.setSize(1001, 500);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.add(panel);
 
@@ -220,9 +262,8 @@ public class CashRegisterGUI implements ActionListener {
             startButton.setBounds(10, 80, 200, 25);
             startButton.addActionListener(new CashRegisterGUI());
             panel.add(startButton);
-            
-            frame.setVisible(true);
-        }            
+        }        
+        
         frame.setVisible(true);       
     }  
 }
